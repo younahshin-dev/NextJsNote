d59396dc6909e7c1eb52ba161cd8d0190a97aa35
younah shin <roaving@gmail.com>
2024-06-05 22:03:38 +0900

    서버컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import {notFound} from 'next/navigation' 
+import os from 'os' //node APIs
+
 const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   
+  console.log(os.hostname());
   return (
     <h1>홈페이지 입니다.</h1>
   )
