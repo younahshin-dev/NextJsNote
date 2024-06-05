47662cbb7691009d97ddc29a6d322f9f0cca1979
younah shin <roaving@gmail.com>
2024-06-05 23:49:08 +0900

    데이터를 읽어서 SSG 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ import styles from './page.module.css'
 import {notFound} from 'next/navigation' 
 import os from 'os' //node APIs
 
+import Counter from '@/components/Counter';
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
-  
+  console.log('안녕 -서버');
   console.log(os.hostname());
   return (
-    <h1>홈페이지 입니다.</h1>
+    <>
+      <h1>홈페이지 입니다.</h1>
+      <Counter />
+    </>
   )
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,19 +1,21 @@
 import Link from 'next/link'
 import styles from './layout.module.css'
+import { getProducts } from '@/service/products';
 
-const products = ['shirt', 'pants', 'outer', 'acc'];
-export default function SlugPage() { 
-return (
-    <section>
-    <h1>제품 소개 페이지</h1>
-    <ul>
-        { 
-            products.map((product, index) => (
-                <li key={index}><Link href={`products/${product}`} >{product}</Link></li>
-            ))
-        }
-       
-    </ul>
-    </section>
-);
+export default async function SlugPage() { 
+    // 서버파일(데이터 베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+    const products = await getProducts();
+    return (
+        <section>
+        <h1>제품 소개 페이지</h1>
+        <ul>
+            { 
+                products.map((product, index) => (
+                    <li key={index}><Link href={`products/${product.id}`} >{product.name}</Link></li>
+                ))
+            }
+        
+        </ul>
+        </section>
+    );
 }
