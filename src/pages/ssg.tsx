fa7600943a258f5374e6e109d4134d862e8fdccf
younahshin <roaving@gmail.com>
2024-06-06 11:59:25 +0900

    next.js 12v ssg, ssr example

---
+++ src/pages/ssg.tsx
@@ -0,0 +1,31 @@
+import MeowArticle from '@/components/MeowArticle';
+import { getProducts, Product } from '@/service/products';
+import Link from 'next/link';
+
+type Props = {
+  products: Product[];
+};
+
+export default function SSGPage({ products }: Props) {
+  return (
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map(({ id, name }, index) => (
+          <li key={index}>
+            <Link href={`/products/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <MeowArticle />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const products = await getProducts();
+  return {
+    props: { products },
+    revalidate: 10,
+  };
+}
---
+++ src/pages/ssr.tsx
@@ -0,0 +1,30 @@
+import MeowArticle from '@/components/MeowArticle';
+import { getProducts, Product } from '@/service/products';
+import Link from 'next/link';
+
+type Props = {
+  products: Product[];
+};
+
+export default function SSGPage({ products }: Props) {
+  return (
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map(({ id, name }, index) => (
+          <li key={index}>
+            <Link href={`/products/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <MeowArticle />
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const products = await getProducts();
+  return {
+    props: { products },
+  };
+}
