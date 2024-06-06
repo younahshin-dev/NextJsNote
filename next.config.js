1e955426ebbf37d781c20bffab7ce8ddf22a1884
younahshin <roaving@gmail.com>
2024-06-06 19:48:19 +0900

    redirect

--- next.config.js
+++ next.config.js
@@ -10,7 +10,21 @@ const nextConfig = {
         hostname:'images.unsplash.com'
       }
     ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/product/deleted_forever',
+        destination: '/products',
+        permanent: true,
+      },
+      {
+        source: '/product/deleted_temp',
+        destination: '/products',
+        permanent: false,
+      }
+    ]
   }
-}
+};
 
 module.exports = nextConfig
